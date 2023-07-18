405f4b2a86ca77d2d956228894f2938b4f628d55
LiviuBuric <112486451+LiviuBuric@users.noreply.github.com>
2023-07-18 14:39:12 +0100

    Add files via upload

---
+++ main.js
@@ -0,0 +1,9 @@
+var Sent1 = "This is the beggining of the string";
+var Sent2 = "  and this is the end of the string";
+var concatenatedString = Sent1 + Sent2;
+function displayAlert() {window.alert(concatenatedString);
+    document.getElementByld("output").textContent = concatenatedString;
+
+}
+
+
